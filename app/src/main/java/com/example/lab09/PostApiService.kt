31df9c9e0f9607925194d9914a0bd47ca03e93db
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2024-09-30 13:07:05 -0500

    Implementación del modelo y la interfaz de la API

---
+++ app/src/main/java/com/example/lab09/PostApiService.kt
@@ -0,0 +1,16 @@
+package com.example.lab09
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface PostApiService {
+    interface PostApiService {
+        @GET("posts")
+        suspend fun getUserPosts(): List<PostModel>
+
+        @GET("posts/{id}")
+        suspend fun getUserPostById(@Path("id") id: Int): PostModel
+
+    }
+
+}
